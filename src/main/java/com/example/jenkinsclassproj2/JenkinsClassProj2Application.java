396ea4e8ee577b7a7a6906c45b3d32f3d2f3105b
Jacobo <valen306@cougars.csusm.edu>
2022-12-05 11:54:15 -0800

    initial commit

---
+++ src/main/java/com/example/jenkinsclassproj2/JenkinsClassProj2Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsclassproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsClassProj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsClassProj2Application.class, args);
+    }
+
+}
