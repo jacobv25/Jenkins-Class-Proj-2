4335c3e82db8de537e414f3cacc93f90c1370d35
Jacobo <valen306@cougars.csusm.edu>
2022-12-05 23:26:21 -0800

    changed port number

--- src/main/java/com/example/jenkinsclassproj2/JenkinsClassProj2Application.java
+++ src/main/java/com/example/jenkinsclassproj2/JenkinsClassProj2Application.java
@@ -10,7 +10,7 @@ import java.util.logging.Logger;
 public class JenkinsClassProj2Application {
     private static Logger logger = Logger.getLogger(JenkinsClassProj2Application.class.getName());
     public static void main(String[] args) {
-        logger.log(Level.INFO, "Oh boy! The application is starting to run...");
+        logger.log(Level.INFO, "\n\nOh boy! The application is starting to run...\n\n");
         SpringApplication.run(JenkinsClassProj2Application.class, args);
     }
 
