db1bd17701367145f5d0fce456facde630161042
Jacobo <valen306@cougars.csusm.edu>
2022-12-05 20:53:04 -0800

    added logs

--- src/main/java/com/example/jenkinsclassproj2/JenkinsClassProj2Application.java
+++ src/main/java/com/example/jenkinsclassproj2/JenkinsClassProj2Application.java
@@ -3,11 +3,14 @@ package com.example.jenkinsclassproj2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 public class JenkinsClassProj2Application {
-
+    private static Logger logger = Logger.getLogger(JenkinsClassProj2Application.class.getName());
     public static void main(String[] args) {
-        // I added a new comment!
+        logger.log(Level.INFO, "Oh boy! The application is starting to run...");
         SpringApplication.run(JenkinsClassProj2Application.class, args);
     }
 
--- src/test/java/com/example/jenkinsclassproj2/JenkinsClassProj2ApplicationTests.java
+++ src/test/java/com/example/jenkinsclassproj2/JenkinsClassProj2ApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.jenkinsclassproj2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootTest
 class JenkinsClassProj2ApplicationTests {
-
+    private Logger logger = Logger.getLogger(JenkinsClassProj2ApplicationTests.class.getName());
     @Test
     void contextLoads() {
+        logger.log(Level.INFO, "This test displays a log message");
     }
 
 }
